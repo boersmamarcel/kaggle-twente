10da4e93bdcf6df9ffd88d5dd6dc7c634c0dc25d
Joost van Doorn <joost.van.doorn@gmail.com>
2013-03-13 18:00:23 +0100

    Svd update

--- svd/svd.R
+++ svd/svd.R
@@ -1,4 +1,5 @@
 ## SVD
 
-train.svd <- data.frame(class(train.clean$pclass), class(train.clean$sex), train.clean$sibsp, train.clean$parch, train.clean$fare, class(train.clean$embarked))
+# train.svd <- data.frame(class(train.clean$pclass), class(train.clean$sex), train.clean$sibsp, train.clean$parch, train.clean$fare, class(train.clean$embarked))
+train.svd <- data.frame(train.clean$sex == "male", train.clean$sex == "female", train.clean$sibsp, train.clean$parch, train.clean$fare)
 model = svd(train.svd, 700, 4)
