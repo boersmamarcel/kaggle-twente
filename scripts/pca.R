1f4482e1c2c1204f334734734febc001eb1d655f
Niek Tax <niektax@gmail.com>
2013-03-19 17:09:15 +0100

    eerste poging met pca. werkt niet zo goed omdat de puur numerieke features eigenlijke niet zoveel zeggen

--- scripts/pca.R
+++ scripts/pca.R
@@ -1,13 +1,76 @@
-t_clean <- data.frame(titanic$survived, titanic$age, titanic$sibsp, titanic$parch, titanic$fare)
-t_clean <- rename(t_clean, c("titanic.survived"="survived","titanic.sibsp"="sibsp","titanic.parch"="parch","titanic.fare"="fare"))))
-sub     <- sample(nrow(t_clean),floor(nrow(t_clean)*0.8))
-train   <- t_clean[sub,]
-test    <- t_clean[-sub,]
-train_0 <- train[train$survived==0,]
-train_1 <- train[train$survived==1,]
-test_0  <- test[test$survived==0,]
-test_1  <- test[test$survived==1,]
-prior_0 <- nrow(train_0)/nrow(train)
-prior_1 <- nrow(train_1)/nrow(train)
-train_no_survived <- train[2:ncol(train)]
-pca     <- princomp(na.omit(train_no_survived))
+# Assumes that getwd() returns git repository folder
+# Use setwd() to change R working directory if this is not the case
+library("plyr")
+
+# Load training and test set
+titanic_train <- read.csv("data/internal_validation/internal_train.csv")
+titanic_test  <- read.csv("data/internal_validation/internal_test.csv")
+train <- data.frame(titanic_train$survived, titanic_train$age, titanic_train$sibsp, titanic_train$parch, titanic_train$fare)
+train <- rename(train, c("titanic_train.survived"="survived","titanic_train.age"="age","titanic_train.sibsp"="sibsp","titanic_train.parch"="parch","titanic_train.fare"="fare"))
+test  <- data.frame(titanic_test$survived, titanic_test$age, titanic_test$sibsp, titanic_test$parch, titanic_test$fare)
+test  <- rename(test, c("titanic_test.survived"="survived","titanic_test.age"="age","titanic_test.sibsp"="sibsp","titanic_test.parch"="parch","titanic_test.fare"="fare"))
+
+# Omit NA's from training and test set
+train <- na.omit(train)
+test  <- na.omit(test)
+
+# Calculate PCA
+pca        <- princomp(na.omit(train[2:ncol(train)]))
+pca_weight <- pca$loadings[,1]
+pca_score  <- pca$scores
+
+# Get indices for survived=0 and survived=1
+train_0 <- rownames(train[train$survived==0,])
+train_1 <- rownames(train[train$survived==1,])
+
+# Calculate priors
+prior_0 <- length(train_0)/nrow(train)
+prior_1 <- length(train_1)/nrow(train)
+
+# Do some conversion stuff, because R is retarded
+train_0_vec <- vector()
+train_1_vec <- vector()
+for(i in 1:nrow(pca_score)) {
+	for(j in 1:length(train_0)){
+		if(rownames(pca_score)[i]==train_0[j]){
+			train_0_vec <- union(train_0_vec, pca_score[i])
+		}
+	}
+	for(j in 1:length(train_1)){
+		if(rownames(pca_score)[i]==train_1[j]){
+			train_1_vec <- union(train_1_vec, pca_score[i])
+		}
+	}
+}
+
+# Calculate new pca feature scores
+test$pca_value <- test$age*pca_weight[1] + test$sibsp*pca_weight[2] + test$parch*pca_weight[3] + test$fare*pca_weight[4] 
+
+# Calculate likelihoods
+test$likelihood_0 <- dnorm(test$pca_value, mean = mean(train_0_vec) , sd = sd(train_0_vec), log = FALSE)
+test$likelihood_1 <- dnorm(test$pca_value, mean = mean(train_1_vec) , sd = sd(train_1_vec), log = FALSE)
+
+# Calculate posteriors
+test$posterior_0 <- test$likelihood_0 * prior_0
+test$posterior_1 <- test$likelihood_1 * prior_1
+
+# Calculate classification
+test$classification <- (test$posterior_1>=test$posterior_0)
+
+# Classification counts
+good = 0;
+bad = 0;
+print("survived -> survived")
+good = good + nrow(test[test$survived==1 & test$classification=='TRUE',])
+good
+print("survived -> died")
+bad = bad + nrow(test[test$survived==1 & test$classification=='FALSE',])
+bad
+print("died -> survived")
+bad = bad + nrow(test[test$survived==0 & test$classification=='TRUE',])
+nrow(test[test$survived==0 & test$classification=='TRUE',])
+print("died -> died")
+good = good + nrow(test[test$survived==0 & test$classification=='FALSE',])
+nrow(test[test$survived==0 & test$classification=='FALSE',])
+print("Accuracy:")
+good/(good+bad)
--- scripts/train-test-splitter.R
+++ scripts/train-test-splitter.R
@@ -1,6 +1,8 @@
-library("plyr")
+# Assumes that getwd() returns git repository folder
+# Use setwd() to change R working directory if this is not the case
+
 sub     <- sample(nrow(titanic),floor(nrow(titanic)*0.8))
 train   <- titanic[sub,]
 test    <- titanic[-sub,]
-write.csv(train, "data/internal validation/internal_train.csv")
-write.csv(test,  "data/internal validation/internal_test.csv")
+write.csv(train, "data/internal_validation/internal_train.csv")
+write.csv(test,  "data/internal_validation/internal_test.csv")
